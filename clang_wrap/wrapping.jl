e877f9c5db32446fd907a4fd07a9160956ded53a
Gerhard Aigner <aigner@trium.de>
2022-01-26 11:33:54 +0100

    peel out code for generating the interface

--- clang_wrap/wrapping.jl
+++ clang_wrap/wrapping.jl
@@ -1,5 +1,10 @@
 # this file contains the code to wrap hidapi using Clang.jl
 
+##############
+# with Clang.jl < 0.14
+##############
+
+
 using hidapi_jll
 using Clang
 
@@ -21,3 +26,20 @@ run(wc)
 # -remove ctypes.jl (not needed in this package)
 # -remove the LibTemplate (we are relying on the _jll to provide the library)
 # -remove "const HID_API_EXPORT_CALL = HID_API_EXPORT" from hidapi_common.jl
+
+##############
+# with Clang.jl >= 0.14
+##############
+
+using Clang.Generators
+using hidapi_jll
+
+cd(@__DIR__)
+
+include_dir = normpath(hidapi_jll.artifact_dir, "include","hidapi")
+options = load_options(joinpath(@__DIR__, "generator.toml"))
+args = get_default_args()
+push!(args, "-I$include_dir")
+headers = [joinpath(include_dir, header) for header in readdir(include_dir) if endswith(header, "hidapi.h")]
+ctx = create_context(headers, args, options)
+build!(ctx)
